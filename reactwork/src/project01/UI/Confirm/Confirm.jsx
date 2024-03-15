27c2873671a214bfadf199d41ea034e607cb8907
Jacob <ljforday0909@gmail.com>
2024-03-16 08:14:49 +1300

    Create Confirm.jsx

---
+++ reactwork/src/project01/UI/Confirm/Confirm.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Bcakdrop from '../backdrop/Bcakdrop'
+import classes from './Confirm.module.css'
+
+export default function Confirm(props) {
+  return (
+   <Bcakdrop className={classes.Confirm}>
+        <div className={classes.ConfirmSure}>
+            <p className={classes.ConfirmText}>{props.ConfirmText}</p>
+            <div>
+                <button onClick={(e)=>{props.onCancel(e)}}>取消</button>
+                <button onClick={(e)=>{props.onOk(e)}}>确认</button>
+            </div>
+        </div>
+   </Bcakdrop>
+  )
+}
